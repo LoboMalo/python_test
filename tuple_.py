2f2c3a884758e65a09931d86f3cfe55249c8a76b
astrocom <astrocom@Davids-MacBook-Pro.local>
2015-05-28 13:12:36 -0400

    Adding all the files fron python test

---
+++ tuple_.py
@@ -0,0 +1,6 @@
+''' Several useful functions for doing things with tuples'''
+def tup_value_w_kwargs( a_tup=(1,2,3), a_int=5 ):
+'''This returns the input tuple with a_int as the first item'''
+    r = (a_int,) +  a_tup[1:]
+    
+    return r
