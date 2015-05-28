42384228c08797f0feaa324bbf4bb4b96c2b817e
astrocom <astrocom@Davids-MacBook-Pro.local>
2015-05-28 14:49:38 -0400

    Added a function to tuples_.py

--- tuples_.py
+++ tuples_.py
@@ -1,6 +1,19 @@
-''' Several useful functions for doing things with tuples'''
+#correct function
+def find_tup_length(*tuples):
+    '''A functipn that takes in an aribtrary amount of tuples and returns their lengths in a list'''
+    
+    
+    list = []
+    for i in tuples:
+           list+= [len(i)]
+    print list        
+    ''' Several useful functions for doing things with tuples'''
+
 def tup_value_w_kwargs( a_tup=(1,2,3), a_int=5 ):
     '''This returns the input tuple with a_int as the first item'''
     r = (a_int,) +  a_tup[1:]
     
     return r
+
+
+
